886a7bf4e1d201829f55b5c95cdba24ffd71da45
chenbin <chenbin@uzoo.cn>
2021-03-09 15:09:51 +0800

    test add

--- src/test/java/com/cb/ShadingjdbcApplicationTests.java
+++ src/test/java/com/cb/ShadingjdbcApplicationTests.java
@@ -1,17 +1,12 @@
 package com.cb;
 
-import com.cb.ShadingjdbcApplication;
 import com.cb.shardingjdbc.entity.User;
-
 import com.cb.shardingjdbc.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
-//import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.UUID;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ShadingjdbcApplication.class)
@@ -23,11 +18,12 @@ class ShadingjdbcApplicationTests {
 
     @Test
     void testAddUser() {
-        for (int i = 0; i < 6; i++) {
+        for (long i = 0; i < 10; i++) {
             User user = new User();
             user.setGender("0");
-            user.setPassword(UUID.randomUUID().toString().substring(0, 5));
+            user.setPassword("00000000000");
             user.setUsername("-->" + i);
+            user.setId(i);
             userMapper.insert(user);
         }
     }
